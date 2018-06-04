e576ae6646439f3c9dc0f3d5ca2b210909cb497f
jj1and <nparticle.science@gmail.com>
2018-06-04 20:55:06 +0900

    ファイルの追加

---
+++ cpp_practice/practice09.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student{
+protected:
+  string name;
+  int id_num;
+  int math;
+  int physics;
+  int english;
+public:
+  Student(string name_init="", int math_init=0, int physics_init=0, int english_init=0 ){
+
+  };
+
+};
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
